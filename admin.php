d5d6b6c63b2578b58c065951b85685106d8cc93e
yanghao <389812249@qq.com>
2018-11-13 16:44:12 +0800

    @thrift

--- admin.php
+++ admin.php
@@ -1,12 +1,3 @@
 <?php
-/*
-* 
-*
-* 
-*
-* 
-*
-* 
- */
 header("Location:/admin/index.php");
 ?>
--- register/index.php
+++ register/index.php
@@ -1,5 +1,6 @@
 <?php
 include(dirname(dirname(__FILE__)).'/global.php');
+include(dirname(dirname(__FILE__))."/include/apiClient.php");
 $Dir = str_replace("/","\\",dirname(__FILE__));
 $DirNameList=explode('\\',$Dir);
 $ModuleName=end($DirNameList);
