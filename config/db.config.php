b2d3e9b6fc51692f8633b929e842dd2751cb3899
yanghao <389812249@qq.com>
2018-11-22 14:32:34 +0800

    @

--- config/db.config.php
+++ config/db.config.php
@@ -1,6 +1,6 @@
 <?php
 
-if (strpos($_SERVER['SERVER_ADDR'], '192.168') !== FALSE) {
+if (strpos($_SERVER['SERVER_ADDR'], '192.168.116.27') !== FALSE) {
     $db_config = array(
         'dbtype' => 'mysql',
         'dbhost' => 'localhost',
@@ -10,7 +10,7 @@ if (strpos($_SERVER['SERVER_ADDR'], '192.168') !== FALSE) {
         'def' => 'phpyun_',
         'charset' => 'GBK',
         'timezone' => 'PRC',
-        'coding' => '64698fc107b59dd7569f2adad3d140c3', 
+        'coding' => '64698fc107b59dd7569f2adad3d140c3',
     );
 
     $db_config1 = array(
@@ -22,9 +22,34 @@ if (strpos($_SERVER['SERVER_ADDR'], '192.168') !== FALSE) {
         'def' => 'huilie_',
         'charset' => 'utf8',
         'timezone' => 'PRC',
-        'coding' => 'utf8', 
+        'coding' => 'utf8',
+    );
+} elseif (strpos($_SERVER['SERVER_ADDR'], '192.168') !== false) {
+    $db_config = array(
+        'dbtype' => 'mysql',
+        'dbhost' => '192.168.116.27',
+        'dbuser' => 'root',
+        'dbpass' => '123456',
+        'dbname' => 'huiliewang',
+        'def' => 'phpyun_',
+        'charset' => 'GBK',
+        'timezone' => 'PRC',
+        'coding' => '64698fc107b59dd7569f2adad3d140c3',
+    );
+
+    $db_config1 = array(
+        'dbtype' => 'mysql',
+        'dbhost' => '192.168.116.27',
+        'dbuser' => 'root',
+        'dbpass' => '123456',
+        'dbname' => 'resume',
+        'def' => 'huilie_',
+        'charset' => 'utf8',
+        'timezone' => 'PRC',
+        'coding' => 'utf8',
     );
 } else {
+
     $db_config = array(
         'dbtype' => 'mysql',
         'dbhost' => '10.30.88.15',
@@ -34,7 +59,7 @@ if (strpos($_SERVER['SERVER_ADDR'], '192.168') !== FALSE) {
         'def' => 'phpyun_',
         'charset' => 'GBK',
         'timezone' => 'PRC',
-        'coding' => '64698fc107b59dd7569f2adad3d140c3', 
+        'coding' => '64698fc107b59dd7569f2adad3d140c3',
     );
 
     $db_config1 = array(
