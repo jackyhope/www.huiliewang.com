6e450b5c469295b3d38d87d176d1dc99956a3ab3
yanghao <389812249@qq.com>
2018-11-28 13:15:33 +0800

    @

--- config/db.config.php
+++ config/db.config.php
@@ -1,6 +1,6 @@
 <?php
 
-if (strpos($_SERVER['SERVER_ADDR'], '192.168.116.27') !== FALSE) {
+if (strpos($_SERVER['SERVER_ADDR'], '192.168.116.27') !== FALSE) {//测试环境1
     $db_config = array(
         'dbtype' => 'mysql',
         'dbhost' => 'localhost',
@@ -24,7 +24,7 @@ if (strpos($_SERVER['SERVER_ADDR'], '192.168.116.27') !== FALSE) {
         'timezone' => 'PRC',
         'coding' => 'utf8',
     );
-} elseif (strpos($_SERVER['SERVER_ADDR'], '192.168') !== false) {
+} elseif (strpos($_SERVER['SERVER_ADDR'], '192.168.116.31') !== FALSE) {//测试环境2
     $db_config = array(
         'dbtype' => 'mysql',
         'dbhost' => '192.168.116.27',
@@ -48,8 +48,31 @@ if (strpos($_SERVER['SERVER_ADDR'], '192.168.116.27') !== FALSE) {
         'timezone' => 'PRC',
         'coding' => 'utf8',
     );
-} else {
+} elseif (strpos($_SERVER['SERVER_ADDR'], '192.168') !== false) {//本地环境
+    $db_config = array(
+        'dbtype' => 'mysql',
+        'dbhost' => '192.168.116.27',
+        'dbuser' => 'root',
+        'dbpass' => '123456',
+        'dbname' => 'huiliewang',
+        'def' => 'phpyun_',
+        'charset' => 'GBK',
+        'timezone' => 'PRC',
+        'coding' => '64698fc107b59dd7569f2adad3d140c3',
+    );
 
+    $db_config1 = array(
+        'dbtype' => 'mysql',
+        'dbhost' => '192.168.116.27',
+        'dbuser' => 'root',
+        'dbpass' => '123456',
+        'dbname' => 'resume',
+        'def' => 'huilie_',
+        'charset' => 'utf8',
+        'timezone' => 'PRC',
+        'coding' => 'utf8',
+    );
+} else {//线上环境
     $db_config = array(
         'dbtype' => 'mysql',
         'dbhost' => '10.30.88.15',
