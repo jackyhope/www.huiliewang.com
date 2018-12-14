aa66544570374f2e4c34ea09fb0b7bcea6f3f54e
yanghao <389812249@qq.com>
2018-12-14 18:23:22 +0800

    @

--- company/index.php
+++ company/index.php
@@ -1,6 +1,7 @@
 <?php
 include(dirname(dirname(__FILE__)).'/global.php');
 include(dirname(dirname(__FILE__))."/include/apiClient.php");
+include(dirname(dirname(__FILE__))."/include/baseUtils.php");
 $Dir = str_replace("/","\\",dirname(__FILE__));
 $DirNameList=explode('\\',$Dir);
 $ModuleName=end($DirNameList);
