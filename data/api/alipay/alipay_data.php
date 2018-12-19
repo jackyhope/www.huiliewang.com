dcea78926cd01445d4340225d66ff78ff7a954b8
yanghao <389812249@qq.com>
2018-12-19 16:56:25 +0800

    @

--- data/api/alipay/alipay_data.php
+++ data/api/alipay/alipay_data.php
@@ -1,4 +1,4 @@
 <?php 
-$alipaydata=array("sy_weburl"=>"http://lt.huiliewang.com","sy_alipayid"=>"2088221230763541","alipaytype"=>"1","sy_alipaycode"=>"yd2ffyh7fgnsl8oab87cbdz18ljmpk6y","sy_alipayemail"=>"yong.zhang@ruc.edu.cn","sy_alipayname"=>"南方新华")
+$alipaydata=array("sy_weburl"=>"http://www.huiliewang.com","sy_alipayid"=>"2088221230763541","alipaytype"=>"1","sy_alipaycode"=>"yd2ffyh7fgnsl8oab87cbdz18ljmpk6y","sy_alipayemail"=>"yong.zhang@ruc.edu.cn","sy_alipayname"=>"南方新华")
 ; 
 ?>
