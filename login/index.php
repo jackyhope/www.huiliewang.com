2f9016f07f1cde66c952b06990b5e1b6d3d484cc
hellocrab <15215087350@163.com>
2019-07-18 19:13:18 +0800

    controller/login/index.class.php and login/index.php入口文件

--- login/index.php
+++ login/index.php
@@ -1,6 +1,10 @@
 <?php
 include(dirname(dirname(__FILE__)).'/global.php');
 include(dirname(dirname(__FILE__))."/include/apiClient.php");
+include(dirname(dirname(__FILE__))."/include/baseUtils.php");
+define('ENV','online');
+define('APPID','');
+define('SECRET','');
 $Dir = str_replace("/","\\",dirname(__FILE__));
 $DirNameList=explode('\\',$Dir);
 $ModuleName=end($DirNameList);
