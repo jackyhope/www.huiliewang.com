7b52051bdd31a5e394d3a245d32ceed4d567df63
langhonglian <626724464@qq.com>
2019-07-27 15:13:00 +0800

    简历详情

--- member/com/model/resume.class.php
+++ member/com/model/resume.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /* *
  * $Author ：PHPYUN开发团队
  *
@@ -8,27 +9,73 @@
  *
  * 软件声明：未经授权前提下，不得用于商业运营、二次开发以及任何形式的再次发布。
  */
-class resume_controller extends company{
-    function index_action(){
-		include(CONFIG_PATH."db.data.php");
-		unset($arr_data['sex'][3]);
-		$this->yunset("arr_data",$arr_data);
-        $uptime=array(1=>'今天',3=>'最近3天',7=>'最近7天',30=>'最近一个月',90=>'最近三个月');
-        $this->yunset('uptime',$uptime);
-        $CacheM=$this->MODEL('cache');
-        $CacheList=$CacheM->GetCache (array('city','user','job','hy'));
-        $date=date("Y",0);
-        $time=date("Y",time());
-        $this->yunset("date",$date);
-        $this->yunset("time",$time);
+
+class resume_controller extends company
+{
+    function index_action() {
+        include(CONFIG_PATH . "db.data.php");
+        unset($arr_data['sex'][3]);
+        $this->yunset("arr_data", $arr_data);
+        $uptime = array(1 => '今天', 3 => '最近3天', 7 => '最近7天', 30 => '最近一个月', 90 => '最近三个月');
+        $this->yunset('uptime', $uptime);
+        $CacheM = $this->MODEL('cache');
+        $CacheList = $CacheM->GetCache(array('city', 'user', 'job', 'hy'));
+        $date = date("Y", 0);
+        $time = date("Y", time());
+        $this->yunset("date", $date);
+        $this->yunset("time", $time);
         $this->yunset($CacheList);
-        $this->yunset("type",$_GET['type']);
+        $this->yunset("type", $_GET['type']);
         $this->public_action();
-        $this->yunset("js_def",8);
+        $this->yunset("js_def", 8);
         $this->com_tpl('resume');
     }
-    function detail_action(){
 
+    function detail_action() {
+//        error_reporting(E_ALL);
+        $resumeId = BaseUtils::getStr($_GET['resume_id'], 'int');
+        $resumeId = 2079009;
+        $projectId = BaseUtils::getStr($_GET['project_id'], 'int');
+        $projectId = 323;
+        $resumeDo = '';
+        $resumeService = '';
+        //简历详情
+        $list = [];
+        try {
+            apiClient::init('', '');
+            $resumeService = new com\hlw\huilie\interfaces\resume\ResumeServiceClient(null);
+            apiClient::build($resumeService);
+            $resumeDo = new com\hlw\huilie\dataobject\resume\resumeRequestDTO();
+            $resumeDo->resume_id = $resumeId;
+            $resumeDo->project_id = $projectId;
+            $info = $resumeService->info($resumeDo);
+
+            if ($info->code != 200) {
+            }
+            $list = json_decode($info->message, true);
+        } catch (Exception $e) {
+            $this->ACT_layer_msg($e->getMessage(), 8);
+        }
+
+        //简历项目操作记录
+        $logs = [];
+        try {
+            $resumeDo->resume_id = $resumeId;
+            $resumeDo->project_id = $projectId;
+            $info = $resumeService->projectLog($resumeDo);
+            if ($info->code != 200) {
+            }
+            $logs = json_decode($info->message, true);
+
+        } catch (Exception $e) {
+        }
+        $list = array_iconv($list, 'utf-8', 'gbk');
+        $logs = array_iconv($logs, 'utf-8', 'gbk');
+        $logs && $logs == array_values($logs);
+
+        $this->yunset("info", $list);
+        $this->yunset("logs", $logs);
+        $this->yunset("status", $list['work_info']['huilie_status']);
         $this->com_tpl('resume_detail');
     }
 }
